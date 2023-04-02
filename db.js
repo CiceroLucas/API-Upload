dadeaec0731ccb63f82c87b5466ea3800ff349f1
CiceroLucas <lukasferreidesousa890@gmail.com>
2023-04-02 11:23:07 -0300

    Criando server e conectando ao Banco de dados

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+require("dotenv").config()
+
+mongoose.set("strictQuery", true)
+
+async function main()
+{
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ahqv4k1.mongodb.net/?retryWrites=true&w=majority`
+    )
+
+    console.log("Conectado com sucesso")
+}
+
+
+main().catch((err) => console.log(err))
+module.exports = main
